8fcb74e195a1f762f33cb1e05286039fd8ceb119
Vicolet <victor.nicolet@heig-vd.ch>
2024-10-14 11:03:51 +0200

    feat: add Picocli dependency and implement initial version command
    - Added Picocli dependency to the Maven project (pom.xml).
    - Implemented initial version command with Picocli (`taget -v` and `tagedit --version`) to display the version of the application.
    - Set up basic command-line structure for future enhancements.

--- TagEdit/src/main/java/ch/heigvd/dai/Main.java
+++ TagEdit/src/main/java/ch/heigvd/dai/Main.java
@@ -1,7 +1,37 @@
 package ch.heigvd.dai;
 
-public class Main {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(
+        name = "tagedit",
+        description = "Print a 'Hello World!' type of message.",
+        version = "TagEdit 0.0.1",
+        mixinStandardHelpOptions = true)
+
+public class Main implements Runnable {
+    @Option(
+            names = {"-v", "--version"},
+            versionHelp = true,
+            description = "Affiche la version de l'application")
+    boolean versionRequested;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public void run() {
+        // Cette méthode sera exécutée si aucune option -v ou --version n'est spécifiée
+        System.out.println("Commande exécutée sans la version. Utilisez -v ou --version pour voir la version.");
     }
 }
